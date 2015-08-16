49ae1ede7de9fada1c43119cb6f941321c649a61
homfen <homfen@qq.com>
2015-08-17 01:46:16 +0800

    theme

--- jekyll/js/categories.js
+++
@@ -1,6 +0,0 @@
-$(".category").bind("click",function(event){
-    event = event||window.event;
-    var target = $(event.target||event.srcElement);
-    var ul = target.siblings("ul");
-    ul.css("display",ul.css("display")=="none"?"block":"none");
-});
