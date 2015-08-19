f6c0327f2111845db901814aa123974919ada501
homfen <homfen@qq.com>
2015-08-19 23:31:20 +0800

    category

--- jekyll/js/categories.js
+++ jekyll/js/categories.js
@@ -1,6 +1,17 @@
-$(".category").bind("click",function(event){
-    event = event||window.event;
-    var target = $(event.target||event.srcElement);
-    var ul = target.siblings("ul");
-    ul.css("display",ul.css("display")=="none"?"block":"none");
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+var category = getParameterByName('category');
+$("div[tag]").each(function(index,div){
+	div = $(div);
+	if(div.attr('tag')==category){
+		div.show();
+	}else{
+		div.remove();
+	}
 });
+
