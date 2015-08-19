90d89c3bc9e2a8eacaaa4b4975363b98673b1f89
homfen <homfen@qq.com>
2015-08-19 22:15:26 +0800

    tags

---
+++ jekyll/js/tags.js
@@ -0,0 +1,15 @@
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+var tag = getParameterByName('tag');
+$("div[tag]").each(function(div){
+	if(div.attr('tag')==tag){
+		div.show();
+	}else{
+		div.remove();
+	}
+});
