0391e218b5fb47239b87327c17c5fffa9ea46e2f
homfen <homfen@qq.com>
2015-08-20 12:58:36 +0800

    scroll

---
+++ jekyll/js/default.js
@@ -0,0 +1,14 @@
+var side = $("#side");
+var sideLeft = side.offset().left;
+function setPosition(){
+	var scrollTop = $(window).scrollTop();
+	var position = side.css("position");
+	if(scrollTop>0 && position=="absolute"){
+		side.css({"position":"fixed","left":sideLeft+"px","top":-scrollTop+"px"});
+		side.animate({"top",0},300);
+	}else if(scrollTop==0 && position=="fixed"){
+		side.css({"position":"absolute","left":0,"top":0});	
+	}
+}
+
+$(window).on("scroll",setPosition);
