de71763a6ccd48ffb8442fcb286436b9ecb7fade
homfen <homfen@qq.com>
2015-08-20 19:38:12 +0800

    side

--- jekyll/js/default.js
+++ jekyll/js/default.js
@@ -2,6 +2,7 @@ var side = $("#side");
 var body = $("body");
 var sideLeft = body.offset().left;
 side.css({"left":sideLeft+"px"});
+side.fadeIn();
 $(window).on("resize",function(){
 	var sideLeft = body.offset().left;
 	side.css({"left":sideLeft+"px"});
