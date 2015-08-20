e874c60936939952bc70b37acf9cebcc89b70975
homfen <homfen@qq.com>
2015-08-20 13:00:59 +0800

    animate

--- jekyll/js/default.js
+++ jekyll/js/default.js
@@ -5,7 +5,7 @@ function setPosition(){
 	var position = side.css("position");
 	if(scrollTop>0 && position=="absolute"){
 		side.css({"position":"fixed","left":sideLeft+"px","top":-scrollTop+"px"});
-		side.animate({"top",0},300);
+		side.animate({"top":0},300);
 	}else if(scrollTop==0 && position=="fixed"){
 		side.css({"position":"absolute","left":0,"top":0});	
 	}
