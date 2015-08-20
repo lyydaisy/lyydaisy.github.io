3e85ae1fc1d76cb034e910a8307f1249f183b839
homfen <homfen@qq.com>
2015-08-20 19:19:53 +0800

    scroll

--- jekyll/js/default.js
+++ jekyll/js/default.js
@@ -2,24 +2,11 @@ var side = $("#side");
 var minHeight = $(window).height();
 side.css("minHeight",minHeight+"px");
 var sideLeft = side.offset().left;
-var ttop = 0;
-var timer = null;
 function setPosition(){
 	var scrollTop = $(window).scrollTop();
-	if(scrollTop!=ttop){
-		ttop = scrollTop;
-		if(timer){
-			clearTimeout(timer);
-		}
-		timer = setTimeout(function(){
-			setPosition();
-		},20);
-		return;
-	}
 	var position = side.css("position");
-	if(scrollTop>100 && position=="absolute"){
-		side.css({"position":"fixed","left":sideLeft+"px","top":-scrollTop+"px"});
-		side.animate({"top":0},300);
+	if(scrollTop>0 && position=="absolute"){
+		side.css({"position":"fixed","left":sideLeft+"px","top":"0px"});
 	}else if(scrollTop==0 && position=="fixed"){
 		side.css({"position":"absolute","left":0,"top":0});	
 	}
