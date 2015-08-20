2a6181a7958abf9eff8712bb5f54c9520c11cf1f
homfen <homfen@qq.com>
2015-08-20 19:26:10 +0800

    side

--- jekyll/js/default.js
+++ jekyll/js/default.js
@@ -1,15 +1,9 @@
 var side = $("#side");
-var minHeight = $(window).height();
-side.css("minHeight",minHeight+"px");
-var sideLeft = side.offset().left;
-function setPosition(){
-	var scrollTop = $(window).scrollTop();
-	var position = side.css("position");
-	if(scrollTop>0 && position=="absolute"){
-		side.css({"position":"fixed","left":sideLeft+"px","top":"0px"});
-	}else if(scrollTop==0 && position=="fixed"){
-		side.css({"position":"absolute","left":0,"top":0});	
-	}
-}
+var body = $("body");
+var sideLeft = body.offset().left;
+side.css({"left":sideLeft+"px"});
+$(window).on("resize",function(){
+	var sideLeft = body.offset().left;
+	side.css({"left":sideLeft+"px"});
+});
 
-$(window).on("scroll",setPosition);
