4d732c5c04bf8cbdc28ace9bea4f9d651bf22d90
homfen <homfen@qq.com>
2015-08-20 19:13:51 +0800

    style

--- jekyll/js/default.js
+++ jekyll/js/default.js
@@ -13,13 +13,13 @@ function setPosition(){
 		}
 		timer = setTimeout(function(){
 			setPosition();
-		},100);
+		},20);
 		return;
 	}
 	var position = side.css("position");
 	if(scrollTop>100 && position=="absolute"){
 		side.css({"position":"fixed","left":sideLeft+"px","top":-scrollTop+"px"});
-		side.animate({"top":0},500);
+		side.animate({"top":0},300);
 	}else if(scrollTop==0 && position=="fixed"){
 		side.css({"position":"absolute","left":0,"top":0});	
 	}
