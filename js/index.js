e348f988d3ac0648bbb687aa1624594a7173bd74
lyydaisy <279359488@qq.com>
2016-01-03 23:56:05 +0800

    animation

--- js/index.js
+++ js/index.js
@@ -4,42 +4,37 @@ $(".lyy").bind("click",function(){
         $(".ball,.crystal").removeClass("ballUp").addClass("ballFall");
         $(".daisy").slideDown();
         setTimeout(function(){
-            $(".ball").removeClass("ballFall ballHide").addClass("ballRotateStart");
-            $(".crystal").removeClass("ballFall crystalHide").addClass("ballRotateStart");
+            $(".ball,.crystal").removeClass('ballHide crystalHide');
+            $(".ball,.crystal").addClass("ballRotateStart");
             setTimeout(function(){
-                $(".ball,.crystal").addClass("ballRotate");
+                $(".ball,.crystal").removeClass('ballRotateStart').addClass("ballRotate");
             },500);
-        },1000);  
-    },500);
+        },1000);
+    },1000);
 });
 $(".crystal").bind("click",function(){
-    $(".ball").removeClass("ballFall ballRotateStart ballRotate").addClass("ballUp ballHide");
-    $(".crystal").removeClass("ballHover ballFall ballRotateStart ballRotate").addClass("ballUp crystalHide");
+    $(".ball,.crystal").removeClass("ballHover ballFall ballRotateStart ballRotate").addClass("ballUp");
     $(".daisy").slideUp();
     setTimeout(function(){
+        $(".ball,.crystal").addClass("ballHide crystalHide");
         $(".sycp,.lyy").hide().removeClass("sycpfade").fadeIn();
     },1000);
 });
-$(".ballText,.crystal").each(function(index,ele){
+$(".ball,.crystal").each(function(index,ele){
     $(ele).bind("mouseenter",function(event){
         var that = $(this);
-        if(that.hasClass("ballText")){
-            that = that.parent();
-        }
-        that.removeClass("ballRotate ballRotateStart").addClass("ballHover");
+        that.removeClass("ballRotate");//.addClass("ballHover");
     }).bind("mouseleave",function(event){
         var that = $(this);
-        if(that.hasClass("ballText")){
-            that = that.parent();
-        }
         if(!that.hasClass("ballHide")&&!that.hasClass("crystalHide")){
-            that.removeClass("ballHover").addClass("ballHoverOut");
-            setTimeout(function(){
-                that.removeClass("ballHoverOut").addClass("ballRotateStart");
+            //that.removeClass("ballHover").addClass("ballHoverOut");
+            //setTimeout(function(){
+                //that.removeClass("ballHoverOut");
+                that.addClass("ballRotateStart");
                 setTimeout(function(){
                     that.removeClass('ballRotateStart').addClass("ballRotate");
                 },500);
-            },1000);
+            //},500);
         }
     });
 });
